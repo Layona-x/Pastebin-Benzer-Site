0b3fd2cf407e0d06a6cbdad3d6d0fc55a1cc83a4
Layona-x <109739779+Layona-x@users.noreply.github.com>
2022-08-21 09:57:32 +0300

    Add files via upload

--- models/text.js
+++ models/text.js
@@ -1,1 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
+const textSchema = new Schema({
+  text:{
+    type:String,
+    require:true,
+  },
+  title:{
+    type:String,
+    require:true,
+  },
+  token:{
+    type:String,
+    require:true,
+  }
+})
+
+const text = mongoose.model("Text",textSchema)
+module.exports = text;
